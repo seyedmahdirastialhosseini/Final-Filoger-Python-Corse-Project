c500f306e124078f5dfddfdfc646b86e87be32e9
Mahdi Rasti <s.mahdirasti@gmail.com>
2025-01-17 12:54:01 +0330

    feat(login): add log files

--- Main.py
+++ Main.py
@@ -1,11 +1,23 @@
 import mysql.connector
 from mysql.connector import errorcode
+import logging
+import datetime
+
+log_filename = "Final-Project.log"
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y - %m - %d %H:%M:%S",
+    handlers=[
+            logging.FileHandler(log_filename, mode="a")]
+    )
+logger = logging.getLogger("DatabaseLogger")
 
 mydb = mysql.connector.connect(
     host="127.0.0.1",
     port=3306,
     user="root",
-    password="Abtin.abtin1",
+    password="Mahdi1388",
     database="databasename",
     auth_plugin="mysql_native_password"
 )
@@ -43,7 +55,7 @@ course = """CREATE TABLE IF NOT EXISTS course (
 
 mycursor.execute(student)
 mycursor.execute(teacher)
-mycursor.execute(course)
+mycursor.execute(course)  
 
 class Student:
     def __init__(self, name, age, email, phone, address):
@@ -59,6 +71,7 @@ class Student:
         mycursor.execute(sql, val)
         mydb.commit()
         print(mycursor.rowcount, "Student inserted.")
+        logger.info(f"Student '{self.name}' added!")
 
     def delete(self, rname):
         sql = "DELETE FROM student WHERE name = %s"
@@ -66,24 +79,28 @@ class Student:
         mycursor.execute(sql, val)
         mydb.commit()
         print(mycursor.rowcount, "Student deleted.")
+        logger.info(f"Student '{rname}' deleted!")
 
     def update(self):
         sql = ("UPDATE student SET name = %s, age = %s, email = %s, phone = %s, address = %s")
         val = (self.name, self.age, self.email, self.phone, self.address)
         mycursor.execute(sql, val)
         mydb.commit()
+        logger.info(f"Student '{self.name}' updated!")
 
     def show(self):
         mycursor.execute("SELECT * FROM student")
         myresult = mycursor.fetchall()
         for x in myresult:
             print(x)
+        logger.info("Showed all student.")
 
     def advance_search_with_query(self, query):
         mycursor.execute(query)
         myresult = mycursor.fetchall()
         for x in myresult:
             print(x)
+        logger.info(f"Advanced search initiated with query: {query}")
 
 class Teacher:
     def __init__(self, name, age, email, phone, lesson, address):
@@ -100,6 +117,7 @@ class Teacher:
         mycursor.execute(sql, val)
         mydb.commit()
         print(mycursor.rowcount, "Teacher inserted.")
+        logger.info(f"Teacher '{self.name}' added!")
 
     def delete(self, rname):
         sql = "DELETE FROM teacher WHERE name = %s"
@@ -107,24 +125,29 @@ class Teacher:
         mycursor.execute(sql, val)
         mydb.commit()
         print(mycursor.rowcount, "Teacher deleted.")
+        logger.info(f"Teacher '{rname}' deleted!")
 
     def update(self):
         sql = ("UPDATE teacher SET name = %s, age = %s, email = %s, phone = %s, lesson = %s, address = %s")
         val = (self.name, self.age, self.email, self.phone, self.lesson, self.address)
         mycursor.execute(sql, val)
         mydb.commit()
+        logger.info(f"Teacher '{self.name}' updated!")
 
     def show(self):
         mycursor.execute("SELECT * FROM teacher")
         myresult = mycursor.fetchall()
         for x in myresult:
             print(x)
+        logger.info("Showed all teachers.")
 
     def advance_search_with_query(self, query):
         mycursor.execute(query)
         myresult = mycursor.fetchall()
         for x in myresult:
             print(x)
+        logger.info("Showed all teachers.")
+        logger.info(f"Advanced search initiated with query: {query}")
 
 class Course:
     def __init__(self, name, time, teacher_id):
@@ -138,6 +161,7 @@ class Course:
         mycursor.execute(sql, val)
         mydb.commit()
         print(mycursor.rowcount, "Course inserted.")
+        logger.info(f"Course '{self.name}' added!")
 
     def delete(self, rname):
         sql = "DELETE FROM course WHERE name = %s"
@@ -145,45 +169,53 @@ class Course:
         mycursor.execute(sql, val)
         mydb.commit()
         print(mycursor.rowcount, "Course deleted.")
+        logger.info(f"Course '{rname}' deleted!")
 
     def update(self):
         sql = ("UPDATE course SET name = %s, time = %s, teacher_id = %s")
         val = (self.name, self.time, self.teacher_id)
         mycursor.execute(sql, val)
         mydb.commit()
+        logger.info(f"Course '{self.name}' updated!")
 
     def show(self):
         mycursor.execute("SELECT * FROM course")
         myresult = mycursor.fetchall()
         for x in myresult:
             print(x)
+        logger.info("Showed all courses.")
 
     def advance_search_with_query(self, query):
         mycursor.execute(query)
         myresult = mycursor.fetchall()
         for x in myresult:
             print(x)
+        logger.info(f"Advanced search initiated with query: {query}")
 
 def panel():
     while True:
-        print("choose what do you want to do ")
-        print('1. Student')
-        print('2. Teacher')
-        print('3. Course')
-        print('4. Exit')
+        print(
+        """Choose what do you want to do with
+        1. Student
+        2. Teacher
+        3. Course
+        4. Exit""")
+
         choice = input("Enter your choice: ")
+        
         if choice == '1':
-            print('what do you want to do with student?')
-            print('1. Add')
-            print('2. Delete')
-            print('3. Update')
-            print('4. Show')
-            print('5. Advance Search')
-            print('6. Back')
+            print("""What do you want to do with your student?
+            1. Add
+            2. Delete
+            3. Update
+            4. Show
+            5. Advance Search
+            6. Back""")
+            
             choice = input("Enter your choice: ")
             if choice == '1':
                 name = input("Enter name: ")
-                age = input("Enter age: ")
+                age = int(input("Enter age: "))
                 email = input("Enter email: ")
                 phone = input("Enter phone: ")
                 address = input("Enter address: ")
@@ -191,7 +223,7 @@ def panel():
                 student.add()
             elif choice == '2':
                 name = input("Enter name: ")
-                student.delete(rname)
+                student.delete(name)
 
             elif choice == '3':
                 name = input("Enter name: ")
@@ -212,16 +244,18 @@ def panel():
                 panel()
             else:
                 print("Invalid choice")
+                logger.warning(f"Choice '{choice}' is invaild.")
 
         elif choice == '2':
-            print('what do you want to do with teacher?')
-            print('1. Add')
-            print('2. Delete')
-            print('3. Update')
-            print('4. Show')
-            print('5. Advance Search')
-            print('6. Back')
+            print("""What do you want to do with your teacher?
+            1. Add
+            2. Delete
+            3. Update
+            4. Show
+            5. Advance Search
+            6. Back""")
             choice = input("Enter your choice: ")
+
             if choice == '1':
                 name = input("Enter name: ")
                 age = input("Enter age: ")
@@ -231,10 +265,12 @@ def panel():
                 address = input("Enter address: ")
                 teacher = Teacher(name, age, email, phone, lesson, address)
                 teacher.add()
+
             elif choice == '2':
                 name = input("Enter name: ")
                 teacher = Teacher(name, '', '', '', '', '')
                 teacher.delete()
+
             elif choice == '3':
                 name = input("Enter name: ")
                 age = input("Enter age: ")
@@ -244,22 +280,30 @@ def panel():
                 address = input("Enter address: ")
                 teacher = Teacher(name, age, email, phone, lesson, address)
                 teacher.update()
+
             elif choice == '4':
                 teacher = Teacher('', '', '', '', '', '')
                 teacher.show()
+
             elif choice == '5':
                 query = input("Enter your query: ")
                 teacher = Teacher('', '', '', '', '', '')
                 teacher.advance_search_with_query(query)
+                
             elif choice == '6':
                 panel()
             else:
                 print("Invalid choice")
-
-
-
-
+                logger.warning(f"Choice '{choice}' is invaild.")
+            
+        elif choice == "3":
+            print("""What do you want to do with your course?
+            1. Add
+            2. Delete
+            3. Update
+            4. Show
+            5. Advance Search
+            6. Back""")
 
 
 panel()
-
